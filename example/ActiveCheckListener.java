6669c0c09f4d04fa5d224d4825078779675e4c7d
Mr-indescribable <st.indescribable@gmail.com>
2020-01-08 10:13:03 +0800

    add more comment for example

--- example/ActiveCheckListener.java
+++ example/ActiveCheckListener.java
@@ -20,6 +20,9 @@ class ReportHandler implements Z4ActiveAgentReportHandler
 	public void handleReport(ZBXActiveAgentReport pkt, InetSocketAddress src)
 		throws InvalidPacket
 	{
+		// you can simply get the packet body in an org.json.JSONObject
+		// object here. For other ways to get attributes, see
+		// ZBXActiveAgentReport class for more details.
 		System.out.println( pkt.getJSON() );
 	}
 }
@@ -31,6 +34,10 @@ public class ActiveCheckListener
 		ZBXACEntry[] acItems = new ZBXACEntry[1];
 		ZBXACEntry item = new ZBXACEntry();
 
+		/**
+		 * lastlogsize and mtime must be given, Zabbix Active check
+		 * protocol defines this and we must follow.
+		 */
 		item.setKey("system.cpu.load[all,avg1]");
 		item.setDelay(1);
 		item.setLastlogsize(0);
@@ -38,6 +45,7 @@ public class ActiveCheckListener
 
 		acItems[0] = item;
 
+		// port: 20051; backlog: 64
 		Z4ActiveAgentListener lsner = new Z4ActiveAgentListener(20051, 64);
 
 		lsner.registerReportHandler(new ReportHandler());
